5bc579ceb930a0d3ba0293078264a69e88923dd7
Steve Jansen <stevejansen_github@icloud.com>
2016-04-20 00:32:23 -0400

    Custom 0.5.2 version metadata

--- version/version.go
+++ version/version.go
@@ -15,7 +15,7 @@ const Version = "0.5.2"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = ""
+const VersionPrerelease = "pr-1318-1335"
 
 // VersionInfo
 type VersionInfo struct {
